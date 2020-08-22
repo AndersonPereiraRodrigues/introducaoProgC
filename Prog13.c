a261e1fbcc23a31d90e38427178ce825f6a35752
Anderson Pereira Rodrigues <anderson.carro.95@gmail.com>
2020-08-22 02:23:44 -0300

    Ler 2 numeros inteiros e imprimi-los

---
+++ Prog13.c
@@ -0,0 +1,18 @@
+//Ler 2 numeros inteiros e imprimi-los
+
+#include<stdio.h>
+
+void main(){
+    int num1,num2;
+
+    printf("Digite numero 1: ");
+    scanf("%d",&num1);
+
+    printf("Digite numero 2: ");
+    scanf("%d",&num2);
+
+    printf("Numero 1: %d\n",num1);
+    printf("Numero 2: %d\n\n",num2);
+
+    printf("Numero 1: %d\nNumero 2: %d\n",num1,num2);
+}
