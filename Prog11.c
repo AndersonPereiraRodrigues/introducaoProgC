ad9a7a204e9a3aaf72e955bf54759615f9019141
Anderson Pereira Rodrigues <anderson.carro.95@gmail.com>
2020-08-22 02:22:42 -0300

    Ler um numero inteiro e imprimi-lo

---
+++ Prog11.c
@@ -0,0 +1,12 @@
+//Ler um numero inteiro e imprimi-lo
+#include<stdio.h>
+
+void main(){
+    int num;
+
+    printf("Digite um numero inteiro: ");
+
+    scanf("%d",&num);
+
+    printf("Numero: %d\n",num);
+}
