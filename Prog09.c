f35676a5892e0d991f4f058b8b80bf9e05b702f0
Anderson Pereira Rodrigues <anderson.carro.95@gmail.com>
2020-08-22 02:21:36 -0300

    Crie um algoritmo que imprima o produto entre 28 e 43 (28*43)

---
+++ Prog09.c
@@ -0,0 +1,14 @@
+//Crie um algoritmo que imprima o produto entre 28 e 43 (28*43)
+#include<stdio.h>
+
+void main(){
+    int produto;
+
+    produto=28*43;
+
+    // Primeiro Modo
+    printf("\nO produto entre os dois e: %d\n",produto);
+
+    // Segundo Modo
+    printf("\nO produto entre os dois e: %d\n",28*43);
+}
