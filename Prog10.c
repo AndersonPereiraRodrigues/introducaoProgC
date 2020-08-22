6a0a500cbbd018def2e5ea26ec265b5bf73f922a
Anderson Pereira Rodrigues <anderson.carro.95@gmail.com>
2020-08-22 02:22:07 -0300

    Crie um algoritmo que imprima a media aritmetica entre os numeros 8, 9 e 7 ((8+9+7)/3)

---
+++ Prog10.c
@@ -0,0 +1,9 @@
+//Crie um algoritmo que imprima a media aritmetica entre os numeros 8, 9 e 7 ((8+9+7)/3)
+#include<stdio.h>
+
+void main(){
+    float ma;
+    ma=(8+3+7)/3.0;
+    printf("\nMedia aritmetica: %0.2f\n",ma);
+    printf("\nMedia aritmetica: %0.2f\n",(8+3+7)/3.0);
+}
