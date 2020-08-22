cfc666fff32db7c0e12009a9d79ede8e7216d4c3
Anderson Pereira Rodrigues <anderson.carro.95@gmail.com>
2020-08-22 02:20:28 -0300

    Imprima a mensagem 'E preciso fazer todos os algoritmos para aprender'

---
+++ Prog07.c
@@ -0,0 +1,7 @@
+//Imprima a mensagem "E preciso fazer todos os algoritmos para aprender"
+
+#include<stdio.h>
+
+void main(){
+        printf("\nE preciso fazer todos os algoritmos para aprender\n");
+}
